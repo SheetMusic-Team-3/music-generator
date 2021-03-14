85a9870a24850af98c01d7b5ed900e0e4ae12a05
James Karsten <james.e.karsten@gmail.com>
2021-03-13 20:06:02 -0500

    Minor updates

--- sheet_music_generator.py
+++ sheet_music_generator.py
@@ -4,6 +4,8 @@ clefs = ('treble', 'soprano', 'mezzosoprano', 'alto', 'tenor', 'baritone', 'bass
 
 notes = ('a', 'b', 'c', 'd', 'e', 'f', 'g')
 
+note_durations = (1, 2, 4, 8, 16, 32)
+
 keys = ('C', 'G', 'D', 'A', 'E', 'B', 'Cb', 'F#', 'Gb', 'Db', 'C#', 'Ab', 'Eb', 'Bb', 'F')
 
 octave_ranges = {
@@ -53,15 +55,17 @@ def generate_line(output_name, num_bars):
         
     # Picks a random key; all keys are written as major
     key = random.choice(keys)
-    key_lp = key[0]
+    key_lp = key[0].lower()
     if key[1:] == '#':
         key_lp += 'is'
     elif key[1:] == 'b':
         key_lp += 'es'
 
-    # Opens 2 output files to write to
+    # Opens 2 output files (.ly and .txt)
     with open(output_name + '.ly', 'w') as lilypond_file:
         with open(output_name + '.txt', 'w') as semantic_file:
+
+            # Writes intro material
             lilypond_file.write('\\version "2.20.0"\\score{{\n')
 
             lilypond_file.write('\\clef ' + clef + '\n')
@@ -72,6 +76,13 @@ def generate_line(output_name, num_bars):
 
             lilypond_file.write(time_lp + '\n')
             semantic_file.write('timeSignature-' + time_semantic + '\n')
+
+            # Writes notes by bar; currently, a note cannot span across a barline
+            for bar in range(num_bars):
+                beats_left = time_bar
+                while beats_left >= 0:
+                    # TODO
+                semantic_file.write('barline\n')
 
 
 
@@ -87,4 +98,5 @@ def multi_generate(num_lines, output_name, num_bars):
     for i in range(num_lines):
         generate_line(output_name + str(i + 1), num_bars)
 
-generate_line('test', 1)
+#generate_line('test', 1)
+#multi_generate(4, 'test', 4)
